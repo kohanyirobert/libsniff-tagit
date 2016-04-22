b4c7549a5c6b950b3f4503d848cccb6c30c28b1d
Robert Kohanyi <robert.kohanyi@metapack.com>
2016-04-22 11:32:22 +0100

    Update libsniff-download dependency, add lib/ and test/ directories
    - Update libsniff-download dependency to 1.0.0, update code to use this
    - Move index.js to lib/download.js, move test.js to test/download.test.js
    - Add index.js that's just exports lib/download.js

--- test/download.test.js
+++ test/download.test.js
@@ -1,12 +1,11 @@
 var assert = require('chai').assert
-var tmp = require('tmp')
 var path = require('path')
-var download = require('./')
-var pkg = require('./package.json')
+var tmp = require('tmp')
+var download = require('../lib/download')
 
 var THIRTY_SECONDS = 30 * 1000
 
-describe(pkg.name, function() {
+describe('download.js', function() {
   it('should download the specfied targets', function(done) {
     this.timeout(THIRTY_SECONDS)
     tmp.dir({unsafeCleanup: true}, function(err, tmpDir) {
@@ -18,15 +17,14 @@ describe(pkg.name, function() {
         ]
       }
       download(options, function(downloads) {
-        assert(downloads)
         assert(downloads.length === options.targets.length)
         downloads.forEach(function(download) {
-          assert(options.targets.indexOf(download.target) > -1)
+          assert(options.targets.indexOf(download.target) != -1)
           assert(download.files.length === 1)
           download.files.forEach(function(file) {
             var destPath = path.resolve(path.join(options.dir, download.target))
             assert(file.indexOf(destPath) === 0)
-            assert(file.indexOf('tagit') > -1)
+            assert(file.indexOf('tagit') != -1)
           })
         })
         done()
